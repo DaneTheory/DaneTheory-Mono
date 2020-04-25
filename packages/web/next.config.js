0cd0ed75ae03ac91e0ef41f113dd215bee3bd1a8
Branden Dane <danetheory@gmail.com>
2020-04-25 07:28:53 -0400

    third commit

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,5 +1,8 @@
 // Tell webpack to compile the "bar" package, necessary if you're using the export statement for example
 // https://www.npmjs.com/package/next-transpile-modules
+
 const withTM = require('next-transpile-modules')(['bar'])
 
-module.exports = withTM()
+module.exports = withTM({
+  distDir: 'build'
+})
